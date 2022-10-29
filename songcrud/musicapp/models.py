56ba977a1af8e7b86e50fdae33c47fea9d034b7d
og-lati <promisesomtoochukwu@gmail.com>
2022-10-29 07:50:11 +0100

    Added models to my app

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,34 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)   
+    title = models.CharField(max_length=200)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+
+    
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=2000)  
+
+
+    def __str__(self):
+        return self.content
